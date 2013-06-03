c5ca2aaf6d4731c991cd09fb777d931f224f1890
boringmachine <machine449@gmail.com>
2013-06-03 15:08:22 +0900

    refactor post controller using 'respond_with'

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,35 +1,24 @@
 class PostsController < ApplicationController
+  respond_to :html, :xml, :json
   # GET /posts
   # GET /posts.json
   def index
     @posts = Post.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @posts }
-    end
+    respond_with(@posts)
   end
 
   # GET /posts/1
   # GET /posts/1.json
   def show
     @post = Post.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @post }
-    end
+    respond_with(@post)
   end
 
   # GET /posts/new
   # GET /posts/new.json
   def new
     @post = Post.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @post }
-    end
+    respond_with(@post)
   end
 
   # GET /posts/1/edit
@@ -41,43 +30,26 @@ class PostsController < ApplicationController
   # POST /posts.json
   def create
     @post = Post.new(params[:post])
-
-    respond_to do |format|
-      if @post.save
-        format.html { redirect_to @post, notice: 'Post was successfully created.' }
-        format.json { render json: @post, status: :created, location: @post }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
-      end
-    end
+    @post[:user_id] = current_user.id;
+    flash[:notice] = 'Post was successfully created.' if @post.save
+    respond_with(@post)
   end
 
   # PUT /posts/1
   # PUT /posts/1.json
   def update
     @post = Post.find(params[:id])
-
-    respond_to do |format|
-      if @post.update_attributes(params[:post])
-        format.html { redirect_to @post, notice: 'Post was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
-      end
+    if(@post[:user_id] == current_user.id)
+      flash[:notice] = 'Post was successfully updated.' if @post.update_attributes(params[:post])
     end
+    respond_with(@post)
   end
 
   # DELETE /posts/1
   # DELETE /posts/1.json
   def destroy
     @post = Post.find(params[:id])
-    @post.destroy
-
-    respond_to do |format|
-      format.html { redirect_to posts_url }
-      format.json { head :no_content }
-    end
+    @post.destroy if @post[:user_id] == current_user.id
+    respond_with(@post)
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,5 @@ class Post < ActiveRecord::Base
   attr_accessible :body, :parent_id, :topic_id, :user_id
   belongs_to :topic
   belongs_to :user
+  scope :recent, order('created_at desc')
 end
