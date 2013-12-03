b9e43d61a5b756312bbac00603eb6e2497be0c41
Shun Sugiyama <shun@localhost.localdomain>
2013-12-04 00:26:41 +0900

    update

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -6,11 +6,6 @@ class Users::SessionsController < Devise::SessionsController
  
   def create
     super
-    group = if Group.first == nil
-      Group.create(name:"Global Group")
-    else
-      Group.first
-    end
-    GroupUser.create(user_id:@user.id,group_id:group.id)
+
   end
 end
