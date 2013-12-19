c8455b3740258f6dbcdd703ff7bb75c07ead6926
Shun Sugiyama <shun@localhost.localdomain>
2013-12-19 23:18:13 +0900

    test

--- db/migrate/20131202070757_remove_category_from_groups.rb
+++
@@ -1,9 +0,0 @@
-class RemoveCategoryFromGroups < ActiveRecord::Migration
-  def up
-    remove_column :groups, :category
-  end
-
-  def down
-    add_column :groups, :category, :integer
-  end
-end
