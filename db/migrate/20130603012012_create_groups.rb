2764242287345baf785a15bb0004d2a70da3d093
Shun Sugiyama <shun@localhost.localdomain>
2013-12-19 22:39:18 +0900

    update

--- db/migrate/20130603012012_create_groups.rb
+++ db/migrate/20130603012012_create_groups.rb
@@ -2,10 +2,6 @@ class CreateGroups < ActiveRecord::Migration
   def change
     create_table :groups do |t|
       t.string :name
-      t.string :url
-      t.string :address
-      t.string :tel
-      t.string :geo
       t.string :description
 
       t.timestamps
