faf4515856fe1290b0a75dd5a761e5649fe03c88
Shun Sugiyama <flank1990@mail.ru>
2014-01-12 21:49:35 +0900

    update

--- db/migrate/20130928073715_remove_user_from_groups.rb
+++
@@ -1,6 +0,0 @@
-class RemoveUserFromGroups < ActiveRecord::Migration
-  def up
-    remove_column :groups, :user_id
-  end
-
-end
--- db/migrate/20131017044137_remove_status_from_group_topic.rb
+++
@@ -1,9 +0,0 @@
-class RemoveStatusFromGroupTopic < ActiveRecord::Migration
-  def up
-    remove_column :group_topics, :status
-  end
-
-  def down
-    add_column :group_topics, :status, :string
-  end
-end
