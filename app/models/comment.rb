eb450d3441394fc0f184cd0a6481b7b8d26e3b9d
Shun Sugiyama <flank1990@mail.ru>
2014-05-01 01:44:08 +0900

    update

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -8,7 +8,7 @@ class Comment < ActiveRecord::Base
     comments.each do |comment|
       child_id = comment[0].to_i
       user = Post.find(child_id).user
-      Mailer.notification(user, body + request.url).deliver
+      Mailer.notification(user, body).deliver
       Comment.create(parent_id: post_id, child_id: child_id) 
     end
   end
