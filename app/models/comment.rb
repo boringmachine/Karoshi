589efc80dc018a3a38d84efb9e0d3f14a41f3eaf
Shun Sugiyama <flank1990@mail.ru>
2014-06-06 14:41:53 +0900

    update

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -7,7 +7,7 @@ class Comment < ActiveRecord::Base
     comments = body.scan(/>>([0-9]+)/).uniq
     users = []
     comments.each do |comment|
-      child_id = comment[0].to_i
+      child_id = comment.first.to_i
       users.push(Post.find(child_id).user)
       Comment.create(parent_id: post_id, child_id: child_id) 
     end
