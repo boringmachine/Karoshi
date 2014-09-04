3c3dfa9b0c5c01b3c064a3a9019011504edccc41
Shun Sugiyama <flank1990@mail.ru>
2014-09-04 13:22:18 +0900

    update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,4 +28,18 @@ class User < ActiveRecord::Base
   has_many :topics, through: :posts
   belongs_to :locale
   
+   
+  def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
+    user = User.where(:provider => auth.provider, :uid => auth.uid).first
+    unless user
+      user = User.create(name:     auth.extra.raw_info.name,
+                         provider: auth.provider,
+                         uid:      auth.uid,
+                         email:    auth.info.email,
+                         password: Devise.friendly_token[0,20]
+                        )
+    end
+    user
+  end
+ 
 end
