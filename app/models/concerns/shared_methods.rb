bd38c8321a9e1b43af2112a65b1bc679d3a10359
Shun Sugiyama <flank1990@mail.ru>
2014-11-30 13:49:09 +0900

    update

--- app/models/concerns/shared_methods.rb
+++ app/models/concerns/shared_methods.rb
@@ -8,16 +8,16 @@ module SharedMethods extend ActiveSupport::Concern
   
   module ClassMethods
     def available_object(id, user_id)
-      obj = find(id)
-      return obj if User.find(user_id) == obj.user
+      obj = find(id).taint
+      return obj.untaint if User.find(user_id) == obj.user
     end
     
     def recent
-      order('created_at desc')
+      order('created_at desc').untaint
     end
  
     def p(page)
-      paginate(page: page, per_page: @per_page)
+      paginate(page: page, per_page: @per_page).untaint
     end
   end
 end
